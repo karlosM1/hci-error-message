0de8b986c5e3459f2f5560642c8bc50aa2438526
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-04-19 04:28:22 +0800

    feat: tanstack set up

---
+++ error/src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+      <ReactQueryDevtools />
+    </>
+  ),
+});
